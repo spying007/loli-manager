342b65e2b0d73bc3ecb1ad68bf59cda510a62d9c
Ritaliez <lijinxia11@post.eurasia.edu>
2018-03-20 17:01:49 +0800

    3.0

--- loli-manager-service/src/main/java/co/loli/service/impl/ContentServiceImpl.java
+++ loli-manager-service/src/main/java/co/loli/service/impl/ContentServiceImpl.java
@@ -2,7 +2,9 @@ package co.loli.service.impl;
 
 import co.loli.pojo.Content;
 import co.loli.service.ContentService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ContentServiceImpl extends BaseServiceImpl<Content> implements ContentService {
 
 }
